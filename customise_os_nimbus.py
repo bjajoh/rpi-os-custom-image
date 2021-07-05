90261713dc933c5e7b7f95071f400f9aa0034c24
Bjarne Johannsen <42101377+bjajoh@users.noreply.github.com>
2021-07-05 18:58:42 +0200

    Update customise_os_nimbus.py

--- customise_os_nimbus.py
+++ customise_os_nimbus.py
@@ -21,9 +21,21 @@ def install_nimbus_apt_dependencies(child):
         "sudo apt-get install -y libxmlsec1-dev libxml2 libxml2-dev libxkbcommon-x11-0 libatlas-base-dev libcurl4-openssl-dev libmicrohttpd-dev libjsoncpp-dev libjsonrpccpp-dev libargtable2-dev libhiredis-dev libi2c-dev libssl-dev libboost-all-dev"
     )
     child.expect_exact(customise_os.BASH_PROMPT, timeout=15*60)
+    child.sendline("echo 'dtoverlay=irs1125' | sudo tee -a /boot/config.txt")
+    child.expect_exact(customise_os.BASH_PROMPT)
+    child.sendline("echo 'deb http://apt.pieye.org/debian/ nimbus-stable main' | sudo tee -a /etc/apt/sources.list")
+    child.expect_exact(customise_os.BASH_PROMPT)
+    child.sendline("wget -O - -q http://apt.pieye.org/apt.pieye.org.gpg.key | sudo apt-key add -")
+    child.expect_exact(customise_os.BASH_PROMPT)
+    child.sendline("sudo apt-get update")
+    child.expect_exact(customise_os.BASH_PROMPT)
+    child.sendline("sudo apt-get install nimbus-server")
+    child.expect_exact(customise_os.BASH_PROMPT)
     child.sendline("df -h")
     child.expect_exact(customise_os.BASH_PROMPT)
-
+    
+    
+    
 
 def run_edits(img_path, needs_login=True):
     print("Staring Raspberry Pi OS Nimbus customisation: {}".format(img_path))
